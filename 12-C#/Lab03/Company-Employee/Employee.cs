7a10c8df4f4778cab147a11d730e9c25010d526d
Mohamed Elbaz <mhmdelbaz57@gmail.com>
2024-01-15 14:10:20 +0200

    C# lab03

---
+++ 12-C#/Lab03/Company-Employee/Employee.cs
@@ -0,0 +1,192 @@
+﻿using System;
+
+
+namespace Company_Employee;
+struct Employee
+{
+    int id;
+    string name;
+    int age;
+    int salary;
+    GenderType gender;
+
+    public int ID
+    {
+        get => id;
+        set
+        {
+            if(value>0 && value<1000)
+            {
+                id = value;
+            }
+            else
+            {
+                id = -1;
+            }
+        }
+    }
+    public string Name
+    {
+        get => name;
+        set
+        {
+            if(! String.IsNullOrEmpty(value))
+            {
+                name= value;
+            }
+            else
+            {
+                name = "No Name";
+            }
+        }
+    }
+    public int Age
+    {
+        get => age;
+        set
+        {
+            if (value > 20 && value < 60)
+            {
+                age = value;
+            }
+            else
+            {
+                age = 20;
+            }
+        }
+    }
+    public int Salary
+    {
+        get => salary;
+        set
+        {
+            if (value > 3000 && value < 50000)
+            {
+                salary = value;
+            }
+            else
+            {
+                salary = 3000;
+            }
+        }
+    }
+
+    public GenderType Gender
+    {
+        get => gender;
+        set => gender = value;
+    }
+    public SecurityLevel SecurityLevel;
+    
+    public HiringDate HiringDate;
+  
+
+    public Employee(int _id ,string _name, int _salary, int _age ,GenderType _gender ,
+                    HiringDate _hiringDate)
+    {
+        ID=_id;
+        Name=_name; 
+        Salary=_salary;
+        Age=_age;
+        Gender =_gender;
+        HiringDate = _hiringDate;    
+    }
+
+    public Employee(int _id, string _name, int _salary, int _age, GenderType _gender)
+            :this(_id, _name, _salary, _age, _gender, new HiringDate(1, 1, DateTime.Now.Year))
+
+    { }
+    public Employee(int _id, string _name, int _salary)
+        :this(_id, _name, _salary, 20, GenderType.M, new HiringDate(1, 1, DateTime.Now.Year))
+
+    {}
+    public Employee(int _id, string _name)
+        : this(_id, _name, 3000, 20, GenderType.M, new HiringDate(1, 1, DateTime.Now.Year))
+    { }
+
+    public override string ToString()
+    {
+        return $"ID ={ID}, Name ={Name}, Salary={Salary} ,Age ={Age} , Gender ={Gender} ,Security Level ={SecurityLevel},Hiring Date {HiringDate}";
+    }
+}
+
+enum GenderType
+{
+    M,F
+}
+
+
+[Flags]
+enum SecurityLevel
+{
+    Guest=1 , Developer=2 , Secretary=4,DBA=8
+}
+struct HiringDate
+{
+    int day;
+    int month;
+    int year;
+
+
+    public int Day
+    {
+        get => day;
+        set
+        {
+            if(value >0 && value <= 31)
+            {
+                day = value;
+            }
+            else
+            {
+                day = 1;
+            }
+        }
+    }
+    public int Month
+    {
+        get => month;
+        set
+        {
+            if (value > 0 && value <= 12)
+            {
+                month = value;
+            }
+            else
+            {
+                month = 1;
+            }
+        }
+    }
+    public int Year
+    {
+        get => year;
+
+        set
+        {
+            if (value > 1980 && value <= DateTime.Now.Year)
+            {
+                year= value;
+            }
+            else
+            {
+                year = DateTime.Now.Year;
+            }
+        }
+    }
+
+    public HiringDate(int _day, int _month, int _year)
+    {
+        Day=_day;
+        Month=_month;
+        Year=_year;
+    }
+
+    public HiringDate(int _month ,int _year) :this(1,_month,_year)
+    {}
+
+    public override string ToString()
+    {
+        return $"{Day}/{Month}/{Year}";
+    }
+}
---
+++ 12-C#/Lab03/Company-Employee/Program.cs
@@ -0,0 +1,91 @@
+﻿using Company_Employee;
+
+Employee[] employees = new Employee[3];
+
+Console.WriteLine("*************Enter Employees Data***********");
+
+for (int i = 0; i < employees.Length; i++)
+{
+    int tempID, tempAge, tempSalary, tempHiringDay, tempHiringMonth, tempHiringYear;
+    string tempName, tempGender;
+    Console.WriteLine($"Employee {i + 1} :\n");
+
+    Console.Write("Enter ID(from 0 to 100) :");
+    while (!int.TryParse(Console.ReadLine(), out tempID) || tempID < 0 || tempID > 100)
+    {
+        Console.WriteLine("Wrong ID!!");
+        Console.WriteLine("Enter ID Again(from 0 to 100) :");
+    }
+
+    Console.Write("Enter Name(more than 2 characters) :");
+    tempName = Console.ReadLine();
+    while (String.IsNullOrEmpty(tempName) || tempName.Length < 3 || int.TryParse(tempName,out int result))
+    {
+        Console.WriteLine("Name is Wrong(more than 2 characters)!!");
+        Console.WriteLine("Enter Name Again :");
+        tempName = Console.ReadLine();
+    }
+
+    Console.Write("Enter Age (from 20 to 60):");
+    while (!int.TryParse(Console.ReadLine(), out tempAge) || tempAge < 20 || tempAge >= 60)
+    {
+        Console.WriteLine("Wrong Age!!");
+        Console.WriteLine("Enter Age Again(from 20 to 60)");
+    }
+
+    Console.Write("Enter Salary (from 3000 to 50000):");
+    while (!int.TryParse(Console.ReadLine(), out tempSalary) || tempSalary < 3000 || tempSalary >= 50000)
+    {
+        Console.WriteLine("Wrong Salary!!");
+        Console.WriteLine("Enter Salary Again(from 3000 to 50000):");
+    }
+
+    Console.Write("Enter Hiring Day (from 1 to 31):");
+    while (!int.TryParse(Console.ReadLine(), out tempHiringDay) || tempHiringDay < 1 || tempHiringDay > 31)
+    {
+        Console.WriteLine("Wrong Hiring Day!!");
+        Console.WriteLine("Enter  Hiring Day Again(from 1 to 31):");
+    }
+
+    Console.Write("Enter Hiring Month (from 1 to 12):");
+    while (!int.TryParse(Console.ReadLine(), out tempHiringMonth) || tempHiringMonth < 1 || tempHiringMonth > 12)
+    {
+        Console.WriteLine("Wrong Hiring Month!!");
+        Console.WriteLine("Enter  Hiring Month Again(from 1 to 12):");
+    }
+
+    Console.Write("Enter Hiring Year (from 1980 to This year):");
+    while (!int.TryParse(Console.ReadLine(), out tempHiringYear) || tempHiringYear < 1980 || tempHiringYear > DateTime.Now.Year)
+    {
+        Console.WriteLine("Wrong Hiring Year!!");
+        Console.WriteLine("Enter   Hiring Year Again(from 1980 to This year):");
+    }
+
+    Console.Write("Enter Gender(M or F) :");
+    tempGender = Console.ReadLine();
+    while (String.IsNullOrEmpty(tempGender) || (tempGender.ToUpper() != "M" && tempGender.ToUpper() != "F"))
+    {
+        Console.WriteLine("Wrong Gender!!");
+        Console.WriteLine("Enter Gender Again(M or F)");
+        tempGender = Console.ReadLine();
+    }
+
+    GenderType genderType = (GenderType)Enum.Parse(typeof(GenderType), tempGender.ToUpper());
+
+
+    employees[i] = new Employee(tempID, tempName, tempSalary, tempAge, genderType,
+                                new HiringDate(tempHiringDay, tempHiringMonth, tempHiringYear));
+
+}
+
+
+
+employees[0].SecurityLevel = SecurityLevel.Guest;
+employees[1].SecurityLevel = SecurityLevel.DBA;
+employees[2].SecurityLevel = SecurityLevel.Guest ^ SecurityLevel.DBA ^ SecurityLevel.Secretary;
+
+
+for (int i = 0; i < employees.Length; i++)
+{
+    Console.WriteLine(employees[i]);
+}
